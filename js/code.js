6fce67dc536eedca5367bc387b2e9b55d51f645c
Neil Ricci <iccir919@gmail.com>
2019-11-21 09:11:36 -0800

    Passes all tests

--- js/code.js
+++ js/code.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
-    let num = Math.floor((Math.random() * data.length));
-    let quote = data[num];
-    $("#text").text(quote.quote);
-    $("footer").text(`${quote.character}, played by ${quote.actor}, in the film ${quote.film}`);
+    let num = undefined;
+    let quote = undefined;
+
+    function newQuote() {
+        num = Math.floor((Math.random() * data.length));
+        quote = data[num];
+        $("#text").text(quote.quote);
+        $("#author").text(createCitation())
+    }
+    newQuote();
+
+    function createCitation() {
+        return `${quote.character}, played by ${quote.actor}, in the film ${quote.film}`;
+    }
+
+    $("#new-quote").click(function(){
+        newQuote();     
+    })
+
+    $("#tweet-quote").click(function(){
+        let text = quote.quote;
+        text += " -" + createCitation();
+        text = encodeURI(text);
+
+        let url = $("tweet-quote").attr(href);
+        url += text;
+        $("tweet-quote").attr(href, url)
+    })
 });
